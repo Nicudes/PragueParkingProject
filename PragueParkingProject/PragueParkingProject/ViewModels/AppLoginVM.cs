e1890373c3613fad53e8e60a07292527610635f4
Daniel Söderberg <dansod85@gmail.com>
2020-05-14 14:34:12 +0200

    AppLogin

---
+++ PragueParkingProject/PragueParkingProject/ViewModels/AppLoginVM.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+ 
+
+namespace PragueParkingProject.ViewModels
+{
+    public class AppLoginVM
+    {
+		private string firstName;
+
+		public string FirstName
+		{
+			get { return firstName; }
+			set { firstName = value; }
+		}
+
+		private int id;
+
+		public int Id
+		{
+			get { return id; }
+			set { id = value; }
+		}
+
+		private string lastName;
+
+		public string LastName
+		{
+			get { return lastName; }
+			set { lastName = value; }
+		}
+
+		private bool isWorking;
+
+		public bool IsWorking
+		{
+			get { return isWorking; }
+			set { isWorking = value; }
+		}
+
+
+	}
+}
---
+++ PragueParkingProject/PragueParkingProject/Views/AppLogin.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PragueParkingProject.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AppLogin : ContentPage
+    {
+        public AppLogin()
+        {
+            InitializeComponent();
+
+            AppUserName.Text = Id.ToString();
+            AppPassword.Text = Services.MockStaff;
+        }
+
+
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
