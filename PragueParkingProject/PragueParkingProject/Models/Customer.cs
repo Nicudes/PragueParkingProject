0ef088f792047f35a0b208b74d182c121ee1a290
André Pontes <33063611+Pottzork@users.noreply.github.com>
2020-05-14 12:12:31 +0200

    Removed list from customer class

--- PragueParkingProject/PragueParkingProject/Models/Customer.cs
+++ PragueParkingProject/PragueParkingProject/Models/Customer.cs
@@ -1,19 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SQLite;
 
 namespace PragueParkingProject.Models
 {
     public class Customer
     {
         //SÄTT PRIMARYKEY OCH AUTOINCREMENT
+        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Phonenumber { get; set; }
         public bool MonthlyCustomer { get; set; }
-        public List<string> RegistrationNumber { get; set; }
+        public string RegistrationNumber { get; set; }
         public string SocialSecurityNumber { get; set; }
 
 
