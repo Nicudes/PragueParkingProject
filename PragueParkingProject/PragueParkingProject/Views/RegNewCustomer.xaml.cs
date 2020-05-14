296b7d35d4191c2f4a70c6c2bb8638fde97dfa43
Mattis <Mattis.vainikainen@gmail.com>
2020-05-14 11:48:24 +0200

    Added DB connection / SQL nugget

---
+++ PragueParkingProject/PragueParkingProject/Views/RegNewCustomer.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PragueParkingProject.Models;
+using SQLite;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PragueParkingProject.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegNewCustomer : ContentPage
+    {
+        public RegNewCustomer()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Clicked(object sender, EventArgs e)
+        {
+            Customer customer = new Customer()
+            {
+                FirstName = nameEntry.Text
+            };
+            using(SQLiteConnection conn = new SQLiteConnection(App.FilePath))
+            {
+                conn.CreateTable<Customer>();
+                conn.Insert(customer);
+            }
+        }
+    }
+}
